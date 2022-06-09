2333ed6a0afccb42f3ed1b392e0935d94df85227
Burak Furkan ERKEMEN <84676805+BFEisHERE@users.noreply.github.com>
2022-06-10 01:22:28 +0300

    Add files via upload

---
+++ Final_tekrar2.java
@@ -0,0 +1,63 @@
+package final_tekrar2;
+import java.util.*;
+import java.io.*;
+public class Final_tekrar2 {
+
+    public static void main(String[] args) throws IOException{
+       
+        /*Scanner giris = new Scanner(System.in);/*
+        boolean tamaMı = true;
+        
+        
+        do {
+            try{
+                System.out.print("Bir tam sayı gir: ");
+                int sayi = giris.nextInt();
+                System.out.println("Girilen Sayı: " + sayi);
+                tamaMı = false;
+            }catch(InputMismatchException ex){
+                System.out.println("Tekrar deneyini");
+                giris.nextLine();
+            }
+        } while (tamaMı);*/
+       
+       
+       
+       /*
+        Scanner giris = new Scanner(System.in);
+        System.out.print("Mesahın: ");
+        String mesaj = giris.nextLine();
+        
+        System.out.print("Kaç Kez Yazdırılsın: ");
+        int n = giris.nextInt();
+        mesajYazdır(mesaj,n," \n");*/
+       
+       /*
+       File dosya = new File("sinif_listesi.txt");
+        if (dosya.exists()) {
+            System.out.println("Dosya mevcut");
+            System.exit(1);
+        }
+        //Dosya oluşturma
+        PrintWriter cikis = new PrintWriter(dosya);
+        //Dosyaya Yazma
+        cikis.print("1-Yaman-Akbulus-Algoritma-");
+        cikis.println(85);
+        cikis.print("2-Diger-Kisi-Algoritma-");
+        cikis.println(90);
+        //dosyayı kapatma
+        cikis.close();*/
+       
+       
+       
+    }  
+    /*public static void mesajYazdır(String msj, int kez,String islem) {
+        for (int i = 0; i < kez; i++) {
+            System.out.print(msj + islem);
+        }*/
+}
+
+
+
+
+   
